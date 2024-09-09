f969c60de9d543f66f6b243f89676772f6dad827
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2024-09-09 09:28:15 +0530

    node upgrade to 18+

--- frontend/src/Components/FloatingIsland/FloatingIsland.jsx
+++ frontend/src/Components/FloatingIsland/FloatingIsland.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Button } from '@mui/material';
 
 export default function FloatingIsland() {
   return (
     <div className='island-drawer'>
-      
+      <Button>
+        <KeyboardArrowUpIcon/>
+      </Button>
     </div>
   )
 }
