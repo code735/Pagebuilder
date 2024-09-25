f1ad377dd536d3ac14684160b0537b2fb74bbd96
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2024-09-25 20:18:28 +0530

    ui changes

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HtmlIcon from '@mui/icons-material/Html';
 
 export default function Navbar() {
+
+const [selectedOption, setselectedOption] = useState("")
+
   return (
     <div className='navbar'>
-      <div className="options-container">
-        <div className="html-icon option-icons">
+      <div className={`options-container ${ selectedOption === 'add-html' && "selected-option"}`}>
+        <div onClick={()=>{
+          selectedOption === 'add-html' ? setselectedOption("") : setselectedOption("add-html")
+        }} className="html-icon add-html option-icons">
           <HtmlIcon />
         </div>
       </div>
