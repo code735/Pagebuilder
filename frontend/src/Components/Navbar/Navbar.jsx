f464fdf234ff730187cb57f09972271f23d0d554
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2024-09-25 21:18:14 +0530

    styling

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -7,10 +7,10 @@ const [selectedOption, setselectedOption] = useState("")
 
   return (
     <div className='navbar'>
-      <div className={`options-container ${ selectedOption === 'add-html' && "selected-option"}`}>
+      <div className={`options-container`}>
         <div onClick={()=>{
           selectedOption === 'add-html' ? setselectedOption("") : setselectedOption("add-html")
-        }} className="html-icon add-html option-icons">
+        }} className={`html-icon add-html option-icons ${ selectedOption === 'add-html' && "selected-option"}`}>
           <HtmlIcon />
         </div>
       </div>
